06fae3a92767bdb7d26bcf1e50df31134b0313e6
KofoCole <kofodym07@yahoo.com>
2024-04-19 10:36:34 +0100

    Edited testimonial

--- src/component/Testimonial/Testimonial.jsx
+++ src/component/Testimonial/Testimonial.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 import "../Testimonial/Testimonial.css";
 import Testimonials from "./Testimonials";
@@ -8,21 +7,8 @@ const Testimonial = () => {
     <div id="testimonial" className="testimonial">
       <h2> What our clients say</h2>
       {/* <div className="testimonial-box"> */}
-          <Testimonials />
-=======
-import style from "../Testimonial/Testimonial.module.css";
-import TestimonialData from "./TestimonialData";
-// import Avatar from "./Avatar";
-
-const Testimonial = () => {
-  return (
-    <div className={style["testimonial"]}>
-      <div className={style["testimonial-box"]}>
-        {/* <Avatar /> */}
-        <h2>What Our Clients Says</h2>
-        <TestimonialData />
->>>>>>> main
-      </div>
+      <Testimonials />
+    </div>
     // </div>
   );
 };
