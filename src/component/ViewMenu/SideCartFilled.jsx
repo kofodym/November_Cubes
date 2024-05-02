d85efe4e5034f309b01ccd431e143d2365b709f9
KofoCole <kofodym07@yahoo.com>
2024-05-02 09:36:13 +0100

    Added checkout icon plus and minus

--- src/component/ViewMenu/SideCartFilled.jsx
+++ src/component/ViewMenu/SideCartFilled.jsx
@@ -1,18 +1,24 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
-// Define the SideCartFilled component
 const SideCartFilled = ({ cartItems }) => {
   return (
     <div className="side-cart-filled">
       <h2 className="text-center">My Cart</h2>
       <ul>
         {cartItems.map((item, index) => (
-          <li key={index}>
-            <img src={item.image} alt={item.alt} />
-            <div>
-              <h3>{item.name}</h3>
-              <p>{item.sub_title}</p>
-              <h4>{item.price}</h4>
+          <li key={index} className="cart-item d-flex g-1">
+            <div className="cart-item-image">
+              <img src={item.image} alt={item.alt} />
+            </div>
+            <div className="cart-item-details">
+              <h3 className="product-name">{item.name}</h3>
+              <h4 className="product-price">{item.price}</h4>
+              <div className="cart-item-controls">
+                <button>-</button>
+                <span>1</span>
+                <button>+</button>
+              </div>
             </div>
           </li>
         ))}
